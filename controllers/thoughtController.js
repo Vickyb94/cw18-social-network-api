cd25818c82d0b3c3b9fe5a2cc98355a92efe09eb
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-03-10 10:59:44 -0400

    added functions to create, delete and update thoughts by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,5 +18,38 @@ module.exports = {
           )
           .catch((err) => res.status(500).json(err));
     },
-    
-}
+    //create a thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thought) => res.json(thought))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+    //delete a thought by id
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+           .then((thought) =>
+              !thought
+              ? res.status(404).json({ message: 'No thought with this ID!' })
+              : User.deleteMany({ _id: { $in: thought.users } } )
+           )
+           .then(() => res.json({ message: 'Deleted!' }))
+           .catch((err) => res.status(500).json(err));
+    },
+    //update a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }  
+        )
+           .then((thought) =>
+           !thought
+              ? res.status(404).json({ message: 'No thought with this ID!' })
+              : res.json(thought)
+        )
+          .catch((err) => res.status(500).json(err));
+    },
+};
