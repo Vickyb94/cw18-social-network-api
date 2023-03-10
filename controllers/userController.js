9da12548de3b74d6086fd2402cd89f1cdcd67026
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-03-10 10:30:06 -0400

    added a function to add and remove friends from a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,5 +37,51 @@ module.exports = {
            )
            .then(() => res.json({ message: 'User and thoughts deleted!' }))
            .catch((err) => res.status(500).json(err));
-    }
-}
+    },
+    //update a user
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+             .then((user) =>
+               !user
+                 ? res.status(404).json({ message: 'No user with that ID' })
+                 : res.json(user)
+             )
+             .catch((err) => res.status(500).json(err));
+    },
+
+    //add a friend to a user
+    addFriend(req, res) {
+        console.log('You are adding a friend!');
+        console.log(req.body);
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+          .then((user) =>
+          !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+    },
+
+    //remove friend from a user
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+            )
+              .then((user) =>
+              !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+    },
+};
